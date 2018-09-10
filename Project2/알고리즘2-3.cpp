85363ea4cabe23ad3600cea1a98231cd344be53e
dayuen <cisspmit@naver.com>
2018-09-10 23:09:56 +0900

    2주차 알고리즘 - 3번 미완성

--- Project2/알고리즘2-3.cpp
+++ Project2/알고리즘2-3.cpp
@@ -1,22 +1,16 @@
+#include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 using namespace std;
-
 
 vector<string> solution(vector<string> strings, int n) {
 	// 1. 문자열 n ~ 전체 인자 비교
 	// 2. n인자가 같을 경우 다음 인자 비교 반복
 	// 3. 작은 순 정리, n ~ 전체 같을 경우 n앞 문자(=전체) 비교
-	bool run(string a, string b) {
-		if (a == b)
-			return a < b;
-		else
-			return a[n] < b[n];
-	}
-
-
-	sort(strings.begin(), strings.end(), run);
+	auto lamba = [&n](string x, string y) ->bool {
+		return (x[n] == y[n])? x.compare(y) : x[n]<y[n];
+	};
+	sort(strings.begin(), strings.end(), lamba);
 	return strings;
 }
