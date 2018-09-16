551aeb0b02da52a7773f58e23c452fa004385f65
zipzaap <cisspmit@naver.com>
2018-09-16 15:04:03 +0900

    Update 알고리즘3-2.cpp

--- Project2/알고리즘3-2.cpp
+++ Project2/알고리즘3-2.cpp
@@ -1,6 +1,4 @@
-#include <string>
-#include <vector>
-#include <algorithm>
+
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -8,16 +6,7 @@
 using namespace std;
 
 string solution(string s) {
-	string answer = "";
-	vector<char> a, b;
-
-	for (int i = 0; i<s.size(); i++)
-		islower(s[i]) ? a.push_back(s[i]) : b.push_back(s[i]);
-
-	sort(a.begin(), a.end(), greater<char>());
+	string answer = s;
 	sort(b.begin(), b.end(), greater<char>());
-	a.insert(a.end(), b.begin(), b.end());
-
-	for (int i = 0; i < a.size(); i++) answer += a[i];
 	return answer;
 }
