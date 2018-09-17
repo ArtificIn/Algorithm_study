dd24223bd4f18c750091bca1690fad440363f73a
zipzaap <cisspmit@naver.com>
2018-09-17 12:31:14 +0900

    Update 알고리즘1-3.cpp

--- Project2/알고리즘1-3.cpp
+++ Project2/알고리즘1-3.cpp
@@ -5,11 +5,6 @@ using namespace std;
 
 vector<int> solution(vector<int> arr)
 {
-	vector<int> answer;
-
 	arr.erase(unique(arr.begin(), arr.end()), arr.end());
-	answer.swap(arr);
-	cout << "Hello Cpp" << endl;
-
-	return answer;
+	return arr;
 }
