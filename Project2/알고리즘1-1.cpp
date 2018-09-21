50d3dec4b0f0da33a361aad95fb6d760b202ca61
zipzaap <cisspmit@naver.com>
2018-09-21 22:42:14 +0900

    Update 알고리즘1-1.cpp

--- Project2/알고리즘1-1.cpp
+++ Project2/알고리즘1-1.cpp
@@ -3,17 +3,11 @@
 using namespace std;
 
 string solution(int a, int b) {
-	if (!((a>0 && a<13) && (b>0 && b<32))) return 0;
-
 	int temp = b;
 	int dayinfo[] = { 31,29,31,30,31,30,31,31,30,31,30,31 };
 	string arr[] = { "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU" };
 
-
-
-	for (int i = 0; i < a - 1; i++) 	temp += dayinfo[i];
-	int tmp = temp % 7 - 1;
-	if (tmp == -1) tmp = 6;
-	string answer = arr[tmp];
-	return answer;
+	for (int i = 0; i < a - 1; i++) temp += dayinfo[i];
+	int tmp = (temp-1) % 7;
+	return arr[tmp];
 }
