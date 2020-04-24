68519482f455a05e817c3e0750247465f2a261e1
artificIn <djgmd1021@gmail.com>
2020-04-24 21:26:04 +0900

    정렬 - h-index

---
+++ algorithm/Programmers/Array/h-index.swift
@@ -0,0 +1,19 @@
+//
+//  h-index.swift
+//  algorithm
+//
+//  Created by 성다연 on 2020/04/23.
+//  Copyright © 2020 성다연. All rights reserved.
+//
+
+import Foundation
+
+//func solution(_ citations:[Int]) -> Int {
+//    let sorted = citations.sorted(by: >)
+//    for i in 0..<sorted.count {
+//        if i >= sorted[i] {
+//            return i
+//        }
+//    }
+//    return sorted.count
+//}
